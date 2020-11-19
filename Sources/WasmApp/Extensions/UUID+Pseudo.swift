f7300fd7841c94268500e9ceba44cc437949e4b8
Mihael Isaev <isaev.mihael@gmail.com>
2020-11-20 01:42:13 +0400

    🕰 Implement UUID pseudo generation
    It maybe not that unique but it works

---
+++ Sources/WasmApp/Extensions/UUID+Pseudo.swift
@@ -0,0 +1,30 @@
+//
+//  UUID+Pseudo.swift
+//  
+//
+//  Created by Mihael Isaev on 18.11.2020.
+//
+
+import Foundation
+
+extension UUID {
+    static func new() -> UUID? {
+        let letters = "ABCDEF0123456789"
+        var result = ""
+        for i in 0...35 {
+            guard ![8, 13, 18, 23].contains(i) else {
+                result.append("-")
+                continue
+            }
+            if i == 14 {
+                result.append("4")
+                continue
+            }
+            let rand = arc4random() % UInt32(letters.count)
+            let ind = Int(rand)
+            let character = letters[letters.index(letters.startIndex, offsetBy: ind)]
+            result.append(character)
+        }
+        return UUID(uuidString: result)
+    }
+}
