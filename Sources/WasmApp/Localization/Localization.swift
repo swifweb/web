5ebf6f6f38e03326366a827a06c743b146bb106b
Mihael Isaev <isaev.mihael@gmail.com>
2020-11-29 23:32:20 +0400

    🇬🇧🇷🇺 Implement Localization

---
+++ Sources/WasmApp/Localization/Localization.swift
@@ -0,0 +1,47 @@
+//
+//  Localization.swift
+//  WasmApp
+//
+//  Created by Mihael Isaev on 29.11.2020.
+//
+
+import Foundation
+
+private var localization = Localization()
+
+public class Localization {
+    let currentLocaleIdentifier = WasmApp.shared.window.navigator.language.replacingOccurrences(of: "-", with: "_")
+    
+    var defaultLanguage: Language = .en
+    
+    lazy var currentLanguage: Language = detectCurrentLanguage()
+    
+    func detectCurrentLanguage() -> Language {
+        if let language = Language(rawValue: currentLocaleIdentifier) {
+            return language
+        }
+        if let language = Language(rawValue: Locale.current.identifier) {
+            return language
+        }
+        if let locale = currentLocaleIdentifier.components(separatedBy: "_").first {
+            if let language = Language(rawValue: locale) {
+                return language
+            } else if let locale = locale.components(separatedBy: "-").first, let language = Language(rawValue: locale) {
+                return language
+            }
+        }
+        return defaultLanguage
+    }
+    
+    static var shared: Localization { localization }
+    
+    public static var current: Language {
+        get { shared.currentLanguage }
+        set { shared.currentLanguage = newValue }
+    }
+    
+    public static var `default`: Language {
+        get { shared.defaultLanguage }
+        set { shared.defaultLanguage = newValue }
+    }
+}
