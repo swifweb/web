7fe0770cf013d7006a0e5c4427d276149e2d8f30
Mihael Isaev <isaev.mihael@gmail.com>
2020-12-19 03:02:23 +0400

    Implement `target` and `href` attributes

--- Sources/WasmApp/Protocols/Attributes/HrefAttrable.swift
+++ Sources/WasmApp/Protocols/Attributes/HrefAttrable.swift
@@ -6,6 +6,55 @@
 //
 
 import Foundation
+import JavaScriptKit
 
-//https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/href
-//<a>, <area>, <base>, <link>    The URL of a linked resource.
+public protocol HrefAttrable {
+    @discardableResult
+    func href(_ value: URLConformable) -> Self
+    @discardableResult
+    func href(_ value: State<URLConformable>) -> Self
+    @discardableResult
+    func href<V>(_ expressable: ExpressableState<V, URLConformable>) -> Self
+}
+
+protocol _HrefAttrable: _AnyElement, HrefAttrable {}
+
+extension HrefAttrable {
+    /// The URL of a linked resource.
+    ///
+    /// Applicable to <a>, <area>, <base>, and <link>
+    ///
+    /// [More info →](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/href)
+    @discardableResult
+    public func href(_ value: URLConformable) -> Self {
+        guard let s = self as? _ActionAttrable else { return self }
+        s.domElement.href = value.stringValue.jsValue()
+        return self
+    }
+    
+    /// The URL of a linked resource.
+    ///
+    /// Applicable to <a>, <area>, <base>, and <link>
+    ///
+    /// [More info →](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/href)
+    @discardableResult
+    public func href(_ value: State<URLConformable>) -> Self {
+        value.listen { self.href($0) }
+        return self
+    }
+    
+    /// The URL of a linked resource.
+    ///
+    /// Applicable to <a>, <area>, <base>, and <link>
+    ///
+    /// [More info →](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/href)
+    @discardableResult
+    public func href<V>(_ expressable: ExpressableState<V, URLConformable>) -> Self {
+        href(expressable.unwrap())
+    }
+}
+
+extension A: _HrefAttrable {}
+extension Area: _HrefAttrable {}
+extension Base: _HrefAttrable {}
+extension Link: _HrefAttrable {}
--- Sources/WasmApp/Protocols/Attributes/TargetAttrable.swift
+++ Sources/WasmApp/Protocols/Attributes/TargetAttrable.swift
@@ -6,6 +6,58 @@
 //
 
 import Foundation
+import JavaScriptKit
 
-//https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/target
-//<a>, <area>, <base>, <form>    Specifies where to open the linked document (in the case of an <a> element) or where to display the response recieved (in the case of a <form> element)
+public protocol TargetAttrable {
+    @discardableResult
+    func target(_ value: Target) -> Self
+    @discardableResult
+    func target(_ value: State<Target>) -> Self
+    @discardableResult
+    func target<V>(_ expressable: ExpressableState<V, Target>) -> Self
+}
+
+protocol _TargetAttrable: _AnyElement, TargetAttrable {}
+
+extension TargetAttrable {
+    /// Specifies where to open the linked document (in the case of an <a> element)
+    /// or where to display the response recieved (in the case of a <form> element)
+    ///
+    /// Applicable to <a>, <area>, <base>, and <form>
+    ///
+    /// [More info →](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/target)
+    @discardableResult
+    public func target(_ value: Target) -> Self {
+        guard let s = self as? _TargetAttrable else { return self }
+        s.domElement.target = value.rawValue.jsValue()
+        return self
+    }
+    
+    /// Specifies where to open the linked document (in the case of an <a> element)
+    /// or where to display the response recieved (in the case of a <form> element)
+    ///
+    /// Applicable to <a>, <area>, <base>, and <form>
+    ///
+    /// [More info →](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/target)
+    @discardableResult
+    public func target(_ value: State<Target>) -> Self {
+        value.listen { self.target($0) }
+        return self
+    }
+    
+    /// Specifies where to open the linked document (in the case of an <a> element)
+    /// or where to display the response recieved (in the case of a <form> element)
+    ///
+    /// Applicable to <a>, <area>, <base>, and <form>
+    ///
+    /// [More info →](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/target)
+    @discardableResult
+    public func target<V>(_ expressable: ExpressableState<V, Target>) -> Self {
+        target(expressable.unwrap())
+    }
+}
+
+extension A: _TargetAttrable {}
+extension Area: _TargetAttrable {}
+extension Base: _TargetAttrable {}
+extension Form: _TargetAttrable {}
