ca54bb1cebef31e5e2a75bacc14517e147355e7a
Mihael Isaev <isaev.mihael@gmail.com>
2021-01-08 19:14:19 +0400

    WIP: implement more properties

--- Sources/WasmApp/CSS/Properties/OutlineProperty.swift
+++ Sources/WasmApp/CSS/Properties/OutlineProperty.swift
@@ -17,21 +17,22 @@ public class OutlineProperty: _Property {
     public var propertyValue: OutlineValue
     var _content = _PropertyContent<OutlineValue>()
     
-    public init<U: UnitValuable>(width: U, style: OutlineStyleType, color: ColorType) {
+    public init<U: UnitValuable>(width: U, style: OutlineStyleType, color: Color) {
         propertyValue = OutlineValue(width: width, style: style, color: color)
     }
 }
 
 extension PropertyKey {
+    /// A shorthand property for the outline-width, outline-style, and the outline-color properties
     public static var outline: PropertyKey<OutlineValue> { "outline".propertyKey() }
 }
 
 public struct OutlineValue: CustomStringConvertible {
     let width: UnitValue
     let style: OutlineStyleType
-    let color: ColorType
+    let color: Color
     
-    public init<U: UnitValuable>(width: U, style: OutlineStyleType, color: ColorType) {
+    public init<U: UnitValuable>(width: U, style: OutlineStyleType, color: Color) {
         self.width = UnitValue(width.value.doubleValue, width.unit)
         self.style = style
         self.color = color
@@ -39,5 +40,18 @@ public struct OutlineValue: CustomStringConvertible {
     
     public var description: String {
         [width.description, style.value, color.description].joined(separator: " ")
+    }
+}
+
+extension Stylesheet {
+    /// A shorthand property for the outline-width, outline-style, and the outline-color properties
+    public typealias Outline = OutlineProperty
+}
+
+extension CSSRulable {
+    /// A shorthand property for the outline-width, outline-style, and the outline-color properties
+    public func outline<U: UnitValuable>(width: U, style: OutlineStyleType, color: Color) -> Self {
+        s?._addProperty(OutlineProperty(width: width, style: style, color: color))
+        return self
     }
 }
