f29bdc502e50ea8382e2f1123925ee32192cb561
Mihael Isaev <isaev.mihael@gmail.com>
2021-01-08 21:54:22 +0400

    WIP: implement more properties

--- Sources/WasmApp/CSS/Properties/GridProperty.swift
+++ Sources/WasmApp/CSS/Properties/GridProperty.swift
@@ -18,23 +18,19 @@ public class GridProperty: _Property {
     public var propertyValue: GridValue
     var _content = _PropertyContent<GridValue>()
     
-    public init (_ value: GridValue) {
-        propertyValue = value
-    }
-    
     /// Specifies the size(s) of the columns and rows
     public init (templateRows: GridTemplateRowsType, templateColumns: GridTemplateColumnsType) {
         propertyValue = GridValue(templateRows: templateRows, templateColumns: templateColumns)
     }
     
     /// Specifies the grid layout using named items
-    public init (_ area: String...) {
+    public init (_ area: [String]) {
         propertyValue = GridValue(area)
     }
     
     /// Specifies the grid layout using named items
-    public init (_ area: [String]) {
-        propertyValue = GridValue(area)
+    public convenience init (_ area: String...) {
+        self.init(area)
     }
     
     /// Specifies the size (height) of the rows, and the auto size of the columns
@@ -58,6 +54,8 @@ public class GridProperty: _Property {
 }
 
 extension PropertyKey {
+    /// A shorthand property for the grid-template-rows, grid-template-columns,
+    /// grid-template-areas, grid-auto-rows, grid-auto-columns, and the grid-auto-flow properties
     public static var grid: PropertyKey<GridValue> { "grid".propertyKey() }
 }
 
@@ -99,4 +97,52 @@ public struct GridValue: CustomStringConvertible {
     }
     
     public var description: String { value }
+}
+
+extension Stylesheet {
+    /// A shorthand property for the grid-template-rows, grid-template-columns,
+    /// grid-template-areas, grid-auto-rows, grid-auto-columns, and the grid-auto-flow properties
+    public typealias Grid = GridProperty
+}
+
+extension CSSRulable {
+    /// Specifies the size(s) of the columns and rows
+    public func grid(templateRows: GridTemplateRowsType, templateColumns: GridTemplateColumnsType) -> Self {
+        s?._addProperty(GridProperty(templateRows: templateRows, templateColumns: templateColumns))
+        return self
+    }
+    
+    /// Specifies the grid layout using named items
+    public func grid(_ area: [String]) -> Self {
+        s?._addProperty(GridProperty(area))
+        return self
+    }
+    
+    /// Specifies the grid layout using named items
+    public func grid(_ area: String...) -> Self {
+        grid(area)
+    }
+    
+    /// Specifies the size (height) of the rows, and the auto size of the columns
+    public func grid(templateRows: GridTemplateRowsType, autoColumns: GridAutoColumnsValue) -> Self {
+        s?._addProperty(GridProperty(templateRows: templateRows, autoColumns: autoColumns))
+        return self
+    }
+    
+    /// Specifies the auto size of the rows, and sets the grid-template-columns property
+    public func grid(autoRows: GridAutoRowsType, templateColumns: GridTemplateColumnsType) -> Self {
+        s?._addProperty(GridProperty(autoRows: autoRows, templateColumns: templateColumns))
+        return self
+    }
+    
+    /// Specifies the size (height) of the rows, and the auto size of the columns
+    public func grid(templateRows: GridTemplateRowsType, autoFlow: GridAutoFlowType, autoColumns: GridAutoColumnsValue) -> Self {
+        s?._addProperty(GridProperty(templateRows: templateRows, autoFlow: autoFlow, autoColumns: autoColumns))
+        return self
+    }
+    
+    public func grid(autoFlow: GridAutoFlowType, autoRows: GridAutoRowsType, templateColumns: GridTemplateColumnsType) -> Self {
+        s?._addProperty(GridProperty(autoFlow: autoFlow, autoRows: autoRows, templateColumns: templateColumns))
+        return self
+    }
 }
--- Sources/WasmApp/CSS/Properties/GridRowProperty.swift
+++ Sources/WasmApp/CSS/Properties/GridRowProperty.swift
@@ -17,25 +17,99 @@ public class GridRowProperty: _Property {
     public var propertyValue: GridRowValue
     var _content = _PropertyContent<GridRowValue>()
     
-    public init (_ value: GridRowValue) {
-        propertyValue = value
+    public init (start: Int, end: Int) {
+        propertyValue = GridRowValue(start: start, end: end)
     }
     
-    public init (start: Int, end: Int) {
+    public init <S>(start: S, end: Int) where S: StateConvertible, S.Value == Int {
+        propertyValue = GridRowValue(start: start, end: end)
+    }
+    
+    public init <E>(start: Int, end: E) where E: StateConvertible, E.Value == Int {
+        propertyValue = GridRowValue(start: start, end: end)
+    }
+    
+    public init <S, E>(start: S, end: E) where S: StateConvertible, S.Value == Int, E: StateConvertible, E.Value == Int {
         propertyValue = GridRowValue(start: start, end: end)
     }
 }
 
 extension PropertyKey {
+    /// A shorthand property for the grid-row-start and the grid-row-end properties
     public static var gridRow: PropertyKey<GridRowValue> { "grid-row".propertyKey() }
 }
 
-public struct GridRowValue: CustomStringConvertible {
-    public let value: String
+public class GridRowValue: CustomStringConvertible, _PropertyValueInnerChangeable {
+    public var value: String
+    
+    var _changeHandler = {}
     
     public init (start: Int, end: Int) {
         value = "\(start) / \(end)"
     }
     
+    public convenience init <S>(start: S, end: Int) where S: StateConvertible, S.Value == Int {
+        let start = start.stateValue
+        self.init(start: start.wrappedValue, end: end)
+        start.listen {
+            self.value = "\($0) / \(end)"
+            self._changeHandler()
+        }
+    }
+    
+    public convenience init <E>(start: Int, end: E) where E: StateConvertible, E.Value == Int {
+        let end = end.stateValue
+        self.init(start: start, end: end.wrappedValue)
+        end.listen {
+            self.value = "\(start) / \($0)"
+            self._changeHandler()
+        }
+    }
+    
+    public convenience init <S, E>(start: S, end: E) where S: StateConvertible, S.Value == Int, E: StateConvertible, E.Value == Int {
+        let start = start.stateValue
+        let end = end.stateValue
+        self.init(start: start.wrappedValue, end: end.wrappedValue)
+        start.listen {
+            self.value = "\($0) / \(end)"
+            self._changeHandler()
+        }
+        end.listen {
+            self.value = "\(start) / \($0)"
+            self._changeHandler()
+        }
+    }
+    
     public var description: String { value }
+}
+
+extension Stylesheet {
+    /// A shorthand property for the grid-row-start and the grid-row-end properties
+    public typealias GridRow = GridRowProperty
+}
+
+extension CSSRulable {
+    /// A shorthand property for the grid-row-start and the grid-row-end properties
+    public func gridRow(start: Int, end: Int) -> Self {
+        s?._addProperty(GridRowProperty(start: start, end: end))
+        return self
+    }
+
+    /// A shorthand property for the grid-row-start and the grid-row-end properties
+    public func gridRow<S>(start: S, end: Int) -> Self where S: StateConvertible, S.Value == Int {
+        s?._addProperty(GridRowProperty(start: start, end: end))
+        return self
+    }
+
+    /// A shorthand property for the grid-row-start and the grid-row-end properties
+    public func gridRow<E>(start: Int, end: E) -> Self where E: StateConvertible, E.Value == Int {
+        s?._addProperty(GridRowProperty(start: start, end: end))
+        return self
+    }
+
+    /// A shorthand property for the grid-row-start and the grid-row-end properties
+    public func gridRow<S, E>(start: S, end: E) -> Self where S: StateConvertible, S.Value == Int, E: StateConvertible, E.Value == Int {
+        s?._addProperty(GridRowProperty(start: start, end: end))
+        return self
+    }
 }
