af3e60f1db7e1a7dac125a4f3618e4880e36d942
Mihael Isaev <isaev.mihael@gmail.com>
2021-01-09 07:55:57 +0400

    WIP: implement more properties

--- Sources/WasmApp/CSS/Properties/FontVariantCapsProperty.swift
+++ Sources/WasmApp/CSS/Properties/FontVariantCapsProperty.swift
@@ -23,33 +23,94 @@ public class FontVariantCapsProperty: _Property {
     public var propertyValue: FontVariantCapsValue
     var _content = _PropertyContent<FontVariantCapsValue>()
     
-    public init (_ value: FontVariantCapsValue) {
-        propertyValue = value
+    public init (_ types: [FontVariantCapsType]) {
+        propertyValue = .init(types)
     }
     
-    public init (_ type: FontVariantCapsType...) {
-        propertyValue = .init(type)
+    public convenience init (_ types: FontVariantCapsType...) {
+        self.init(types)
     }
     
-    public init (_ type: [FontVariantCapsType]) {
-        propertyValue = .init(type)
+    public convenience init <V>(_ types: V) where V: StateConvertible, V.Value == [FontVariantCapsType] {
+        let types = types.stateValue
+        self.init(types.wrappedValue)
+        types.listen {
+            self._changed(to: .init($0))
+        }
+    }
+    
+    public convenience init <V>(_ types: V) where V: StateConvertible, V.Value == FontVariantCapsType {
+        let types = types.stateValue
+        self.init(types.wrappedValue)
+        types.listen {
+            self._changed(to: .init($0))
+        }
     }
 }
 
 extension PropertyKey {
+    /// Controls the usage of alternate glyphs for capital letters
     public static var fontVariantCaps: PropertyKey<FontVariantCapsValue> { "font-variant-caps".propertyKey() }
 }
 
-public struct FontVariantCapsValue: CustomStringConvertible {
-    public let value: String
+public class FontVariantCapsValue: CustomStringConvertible, _PropertyValueInnerChangeable {
+    public var value: String
     
-    public init (_ type: FontVariantCapsType...) {
-        value = type.map { $0.value }.joined(separator: " ")
-    }
+    var _changeHandler = {}
     
     public init (_ type: [FontVariantCapsType]) {
         value = type.map { $0.value }.joined(separator: " ")
     }
     
+    public convenience init (_ type: FontVariantCapsType...) {
+        self.init(type)
+    }
+    
+    public convenience init <V>(_ types: V) where V: StateConvertible, V.Value == [FontVariantCapsType] {
+        let types = types.stateValue
+        self.init(types.wrappedValue)
+        types.listen {
+            self.value = $0.map { $0.value }.joined(separator: " ")
+        }
+    }
+    
+    public convenience init <V>(_ types: V) where V: StateConvertible, V.Value == FontVariantCapsType {
+        let types = types.stateValue
+        self.init(types.wrappedValue)
+        types.listen {
+            self.value = $0.value
+        }
+    }
+    
     public var description: String { value }
+}
+
+extension Stylesheet {
+    /// Controls the usage of alternate glyphs for capital letters
+    public typealias FontVariantCaps = FontVariantCapsProperty
+}
+
+extension CSSRulable {
+    /// Controls the usage of alternate glyphs for capital letters
+    public func fontVariantCaps(_ types: [FontVariantCapsType]) -> Self {
+        s?._addProperty(FontVariantCapsProperty(types))
+        return self
+    }
+    
+    /// Controls the usage of alternate glyphs for capital letters
+    public func fontVariantCaps(_ types: FontVariantCapsType...) -> Self {
+        fontVariantCaps(types)
+    }
+    
+    /// Controls the usage of alternate glyphs for capital letters
+    public func fontVariantCaps<V>(_ types: V) -> Self where V: StateConvertible, V.Value == [FontVariantCapsType] {
+        s?._addProperty(FontVariantCapsProperty(types))
+        return self
+    }
+    
+    /// Controls the usage of alternate glyphs for capital letters
+    public func fontVariantCaps<V>(_ types: V) -> Self where V: StateConvertible, V.Value == FontVariantCapsType {
+        s?._addProperty(FontVariantCapsProperty(types))
+        return self
+    }
 }
