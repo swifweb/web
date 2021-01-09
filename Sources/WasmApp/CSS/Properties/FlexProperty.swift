80d9a7424307ab24715639225c225f87ea4ee430
Mihael Isaev <isaev.mihael@gmail.com>
2021-01-09 08:25:53 +0400

    WIP: implement more properties

--- Sources/WasmApp/CSS/Properties/FlexProperty.swift
+++ Sources/WasmApp/CSS/Properties/FlexProperty.swift
@@ -14,29 +14,10 @@
 ///
 /// [Learn more](https://www.w3schools.com/cssref/css3_pr_flex.asp)
 public class FlexProperty: _Property {
-    public class MS: _Property {
-        public var propertyKey: PropertyKey<FlexValue> { .flex_ms }
-        public var propertyValue: FlexValue
-        var _content = _PropertyContent<FlexValue>()
-        
-        public init (_ value: FlexValue) {
-            propertyValue = value
-        }
-    
-        public init<U: UnitValuable>(grow: NumericValue, shrink: NumericValue, basis: U) {
-            propertyValue = .init(grow: grow, shrink: shrink, basis: basis)
-        }
-    }
-    
     public var propertyKey: PropertyKey<FlexValue> { .flex }
     public var propertyValue: FlexValue
     public var propertyAliases: [AnyProperty]
     var _content = _PropertyContent<FlexValue>()
-    
-    public init (_ value: FlexValue) {
-        propertyValue = value
-        propertyAliases = [MS(value)]
-    }
     
     public init<U: UnitValuable>(grow: NumericValue, shrink: NumericValue, basis: U) {
         propertyValue = .init(grow: grow, shrink: shrink, basis: basis)
@@ -45,8 +26,10 @@ public class FlexProperty: _Property {
 }
 
 extension PropertyKey {
+    /// A shorthand property for the flex-grow, flex-shrink, and the flex-basis properties
+    public static var flex: PropertyKey<FlexValue> { "flex".propertyKey() }
+    /// A shorthand property for the flex-grow, flex-shrink, and the flex-basis properties
     public static var flex_ms: PropertyKey<FlexValue> { "-ms-flex".propertyKey() }
-    static var flex: PropertyKey<FlexValue> { "flex".propertyKey() }
 }
 
 public struct FlexValue: CustomStringConvertible {
@@ -57,4 +40,29 @@ public struct FlexValue: CustomStringConvertible {
     }
     
     public var description: String { value }
+}
+
+extension Stylesheet {
+    /// A shorthand property for the flex-grow, flex-shrink, and the flex-basis properties
+    public typealias Flex = FlexProperty
+}
+
+extension CSSRulable {
+    /// A shorthand property for the flex-grow, flex-shrink, and the flex-basis properties
+    public func flex<U: UnitValuable>(grow: NumericValue, shrink: NumericValue, basis: U) -> Self {
+        s?._addProperty(FlexProperty(grow: grow, shrink: shrink, basis: basis))
+        return self
+    }
+}
+
+extension FlexProperty {
+    public class MS: _Property {
+        public var propertyKey: PropertyKey<FlexValue> { .flex_ms }
+        public var propertyValue: FlexValue
+        var _content = _PropertyContent<FlexValue>()
+        
+        public init<U: UnitValuable>(grow: NumericValue, shrink: NumericValue, basis: U) {
+            propertyValue = .init(grow: grow, shrink: shrink, basis: basis)
+        }
+    }
 }
