72b17524add6f4caa98a662e36a40ab7202aebf0
Mihael Isaev <isaev.mihael@gmail.com>
2021-01-09 08:57:37 +0400

    WIP: implement more properties

--- Sources/WasmApp/CSS/Properties/GridAreaProperty.swift
+++ Sources/WasmApp/CSS/Properties/GridAreaProperty.swift
@@ -18,10 +18,6 @@ public class GridAreaProperty: _Property {
     public var propertyValue: GridAreaValue
     var _content = _PropertyContent<GridAreaValue>()
     
-    public init (_ value: GridAreaValue) {
-        propertyValue = value
-    }
-    
     public init (rowStart: GridRowStartValue, columnStart: GridColumnStartValue, rowEnd: GridRowEndValue, columnEnd: GridColumnEndValue) {
         propertyValue = .init(rowStart: rowStart, columnStart: columnStart, rowEnd: rowEnd, columnEnd: columnEnd)
     }
@@ -29,9 +25,19 @@ public class GridAreaProperty: _Property {
     public init (_ area: String) {
         propertyValue = .init(area)
     }
+    
+    public convenience init <S>(_ area: S) where S: StateConvertible, S.Value == String {
+        let area = area.stateValue
+        self.init(area.wrappedValue)
+        area.listen {
+            self._changed(to: .init($0))
+        }
+    }
 }
 
 extension PropertyKey {
+    /// Either specifies a name for the grid item, or this property is a shorthand property
+    /// for the grid-row-start, grid-column-start, grid-row-end, and grid-column-end properties
     public static var gridArea: PropertyKey<GridAreaValue> { "grid-area".propertyKey() }
 }
 
@@ -47,4 +53,33 @@ public struct GridAreaValue: CustomStringConvertible {
     }
     
     public var description: String { value }
+}
+
+extension Stylesheet {
+    /// Either specifies a name for the grid item, or this property is a shorthand property
+    /// for the grid-row-start, grid-column-start, grid-row-end, and grid-column-end properties
+    public typealias GridArea = GridAreaProperty
+}
+
+extension CSSRulable {
+    /// Either specifies a name for the grid item, or this property is a shorthand property
+    /// for the grid-row-start, grid-column-start, grid-row-end, and grid-column-end properties
+    public func gridArea(rowStart: GridRowStartValue, columnStart: GridColumnStartValue, rowEnd: GridRowEndValue, columnEnd: GridColumnEndValue) -> Self {
+        s?._addProperty(GridAreaProperty(rowStart: rowStart, columnStart: columnStart, rowEnd: rowEnd, columnEnd: columnEnd))
+        return self
+    }
+    
+    /// Either specifies a name for the grid item, or this property is a shorthand property
+    /// for the grid-row-start, grid-column-start, grid-row-end, and grid-column-end properties
+    public func gridArea(_ area: String) -> Self {
+        s?._addProperty(GridAreaProperty(area))
+        return self
+    }
+    
+    /// Either specifies a name for the grid item, or this property is a shorthand property
+    /// for the grid-row-start, grid-column-start, grid-row-end, and grid-column-end properties
+    public func gridArea<S>(_ area: S) -> Self where S: StateConvertible, S.Value == String {
+        s?._addProperty(GridAreaProperty(area))
+        return self
+    }
 }
