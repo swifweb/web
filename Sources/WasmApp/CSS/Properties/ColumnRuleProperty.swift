3692e20966bf1f6dcd8e4ba4dc9dd3613162fb7b
Mihael Isaev <isaev.mihael@gmail.com>
2021-01-09 08:29:38 +0400

    WIP: implement more properties

--- Sources/WasmApp/CSS/Properties/ColumnRuleProperty.swift
+++ Sources/WasmApp/CSS/Properties/ColumnRuleProperty.swift
@@ -17,25 +17,35 @@ public class ColumnRuleProperty: _Property {
     public var propertyValue: ColumnRuleValue
     var _content = _PropertyContent<ColumnRuleValue>()
     
-    public init (_ value: ColumnRuleValue) {
-        propertyValue = value
-    }
-    
-    public init (width: ColumnRuleWidthType, style: ColumnRuleStyleType, color: ColorType) {
+    public init (width: ColumnRuleWidthType, style: ColumnRuleStyleType, color: Color) {
         propertyValue = ColumnRuleValue(width: width, style: style, color: color)
     }
 }
 
 extension PropertyKey {
+    /// A shorthand property for all the column-rule-* properties
     public static var columnRule: PropertyKey<ColumnRuleValue> { "column-rule".propertyKey() }
 }
 
 public struct ColumnRuleValue: CustomStringConvertible {
     public let value: String
     
-    public init (width: ColumnRuleWidthType, style: ColumnRuleStyleType, color: ColorType) {
+    public init (width: ColumnRuleWidthType, style: ColumnRuleStyleType, color: Color) {
         value = [width.value, style.value, color.description].joined(separator: " ")
     }
     
     public var description: String { value }
+}
+
+extension Stylesheet {
+    /// A shorthand property for all the column-rule-* properties
+    public typealias ColumnRule = ColumnRuleProperty
+}
+
+extension CSSRulable {
+    /// A shorthand property for all the column-rule-* properties
+    public func columnRule(width: ColumnRuleWidthType, style: ColumnRuleStyleType, color: Color) -> Self {
+        s?._addProperty(ColumnRuleProperty(width: width, style: style, color: color))
+        return self
+    }
 }
