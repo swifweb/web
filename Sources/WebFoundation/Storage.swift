5d3199e55e5612d2f8dd3220c114bdfdde12e4f8
Mikhail Isaev <isaev.mihael@gmail.com>
2026-01-26 09:21:16 +0400

    🪚 Implement Storage sendability

--- Sources/WebFoundation/Storage.swift
+++ Sources/WebFoundation/Storage.swift
@@ -5,9 +5,9 @@
 //  Created by Mihael Isaev on 24.02.2021.
 //
 
-import Foundation
+import FoundationEssentials
 
-public struct Storage {
+public struct Storage: Sendable {
     var storage: [ObjectIdentifier: AnyStorageValue]
 
     struct Value<T>: AnyStorageValue {
